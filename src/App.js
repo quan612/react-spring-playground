b99f51e7c541b25c5e4e050c36a9398f0b7cccd7
quan612 <quan612@yahoo.com>
2020-01-23 17:53:44 -0500

    initialize

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useSpring, animated } from "react-spring";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const animation = useSpring({
+    from: { opacity: 0 },
+    to: { opacity: 1 },
+    delay: 1000,
+    config: { duration: 1000 }
+  });
+
+  const colorAnimation = useSpring({
+    from: { color: "blue" },
+    to: { color: `rgb(255,0,0)` },
+    config: { duration: 1000 }
+  });
+
+  const multiAnimation = useSpring({
+    from: { opacity: 0, color: "red" },
+    to: [
+      { opacity: 1, color: "#ffaaee" },
+      { opacity: 0 },
+      { opacity: 1, color: "red" },
+      { opacity: 0 },
+      { opacity: 0.5, color: "#008000" },
+      { opacity: 0.8, color: "black" }
+    ],
+    delay: 1000,
+    config: { duration: 1000 }
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <animated.h1 style={animation}>Hi Quan</animated.h1>
+      <animated.h1 style={colorAnimation}>Hi Bon Bon</animated.h1>
+      <animated.h1 style={multiAnimation}>Hi Ngoc</animated.h1>
     </div>
   );
-}
+};
 
 export default App;
