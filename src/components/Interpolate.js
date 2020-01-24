9803b0904a51c2e34e8e303eb0fcabf7e4314fa4
quan612 <quan612@yahoo.com>
2020-01-24 17:45:53 -0500

    test

--- src/components/Interpolate.js
+++ src/components/Interpolate.js
@@ -5,18 +5,37 @@ import styled from "styled-components";
 const Interpolate = () => {
   const [on, toggle] = useState(false);
 
-  const { xy } = useSpring({
-    from: { xy: [-500, -500], c: "green" },
-    xy: on ? [800, 200] : [0, 0],
-    c: on ? "red" : "green"
+  const { x, c } = useSpring({
+    from: { x: [0, 0], c: "green" },
+    x: on ? 1 : 0,
+    c: on ? 1 : 0,
+    config: {
+      mass: 50,
+      tension: 40,
+      friction: 60,
+      clamp: false,
+      duration: 2500
+    }
   });
 
   return (
     <div>
       <animated.h1
         style={{
-          transform: xy.interpolate((x, y) => `translate(${x}px, ${y}px)`),
-          color: xy.interpolate()
+          //  transform: xy.interpolate((x, y) => `translate(${x}px, ${y}px)`),
+          // color: c.interpolate(c => c)
+          transform: x
+            .interpolate({
+              range: [0, 0.25, 0.5, 0.75, 1],
+              output: [0, 500, 200, 800, 500]
+            })
+            .interpolate(x => `translateX(${x}px)`),
+          color: c
+            .interpolate({
+              range: [0, 0.5, 1],
+              output: ["red", "green", "blue"]
+            })
+            .interpolate(c => c)
         }}
       >
         {!on ? "Here" : "There"}
